f897d921d6558daaeecc1a4e3a5530a6a0a9b39c
Benoit Vrins <benoit@spade.be>
2016-12-21 10:40:02 +0100

    Homepage building, HTML structure seems to be OK

--- index.php
+++ index.php
@@ -51,20 +51,146 @@
         
         <link rel="stylesheet" href="assets/css/app.css">
         
+        <style >
+	        .wrapper1{
+		        border-top:1px solid #ccc;
+		        padding: 50px 20px;
+		        background-color: #eee;
+	        }
+	        .wrapper2{
+		        border-top:1px solid #ccc;
+		        padding: 50px 20px;
+		        background-color: #fff;
+	        }
+	        .center{
+		        margin:0 auto;
+	        }
+	        .center > *{
+		        text-align: center;
+	        }
+        </style>
+        
+        
     </head>
     
     <body>
-	    <div class="pure-g">
-        <div class="pure-u-1">
-	        <h1>Ce que voient les daltoniens</h1>
-        </div>
-    	</div>
+	    <!-- -->
+	    <header id="header" class="">
+		    <nav class="pure-menu pure-menu-horizontal">
+        	<a href="#" class="pure-menu-heading pure-menu-link">Visionary</a>
+	        <ul class="pure-menu-list">
+	          <li id="menu-item-1" class="pure-menu-item"><a href="#" class="pure-menu-link">About</a></li>
+	          <li id="menu-item-2" class="pure-menu-item"><a href="#" class="pure-menu-link">Colour blindness</a></li>
+	          <li id="menu-item-3" class="pure-menu-item"><a href="#" class="pure-menu-link">Guide</a></li>
+	          <li id="menu-item-4" class="pure-menu-item"><a href="#" class="pure-menu-link">Download</a></li>
+	          <li id="menu-item-5" class="pure-menu-item"><a href="#" class="pure-menu-link">Contact</a></li>
+	        </ul>
+		    </nav>
+	    </header>
+      
+	    <!-- -->
+	    <section id="hero" class="wrapper1">
+		    <div class="pure-g">
+	        <div class="pure-u-1 center">
+		        <h1>Ce que voient les daltoniens</h1>
+		        <p>Nous n’avons pas tous la même perception des couleurs. Visionary travaille à améliorer le confort des utilisateurs daltoniens face aux interfaces digitales </p>
+		        <p><a href="#" class="pure-button pure-button-primary">Voir la vidéo</a></p>
+	        </div>
+	    	</div>
+	    </section>
     	
-    	<div class="pure-g">
-        <div class="pure-u-1-3"><p>Thirds</p></div>
-        <div class="pure-u-1-3"><p>Thirds</p></div>
-        <div class="pure-u-1-3"><p>Thirds</p></div>
-    	</div>
+    	<!-- -->
+    	<section id="content-bloc-1" class="wrapper2">
+	    	<div class="pure-g">
+	        <div class="pure-u-2-3">
+		        <h2>Daltoniens et interfaces digitales</h2>
+		        <p>Smartphones, écrans tactiles de commande dans les gares, les métros, portables, tablets… La conception de ces interfaces sont-elles réellement accessibles à tous ? Les concepteurs tiennent-ils compte des différences de perception des couleurs ? Ces questions sont à l’origine de la mise en place de différents outils pour améliorer la situation.</p>
+		      </div>
+	        <div class="pure-u-1-3">
+		        <img src="assets/images/placeholder.png" class="pure-img">
+	        </div>
+    		</div>
+	    	<div class="pure-g">
+	        <div class="pure-u-1-3">
+		        <h3>Comment voient les daltoniens ?</h3>
+		        <p>Un homme sur douze a une perception des couleurs anormale.  Parfois, c’est un avantage.</p>
+		        <p><a href="#" class="pure-button pure-button-primary">Plus sur le daltonimse</a></p>
+		      </div>
+	        <div class="pure-u-1-3">
+		        <h3>Concevoir des écrans accessibles aux daltoniens ?</h3>
+		        <p>Designers, oui c’est possible ! Visionary a met disposition un guide à destination des concepteurs d’interfaces. Facile.</p>
+		        <p><a href="#" class="pure-button pure-button-primary">Lire le guide</a></p>
+		      </div>
+	        <div class="pure-u-1-3">
+		        <h3>Etes-vous daltonien ?</h3>
+		        <p>Teste notre solution logicielle gratuite pour améliorer les interfaces digitales.</p>
+		        <p><a href="#" class="pure-button pure-button-primary">Tester gratuitement</a></p>
+		      </div>
+	    	</div>
+    	</section>
     	
+    	<!-- -->
+    	<section id="content-bloc-2" class="wrapper1">
+	    	<div class="pure-g">
+	        <div class="pure-u-1-2">
+		        <h2>A propos de Visionary</h2>
+		        <p>Visionary est un projet de recherche de solutions améliorant l’accessibilité du web pour les personnes souffrant de déficiences visuelles et, en particulier, le daltonisme.</p>
+		        <p><a href="#" class="pure-button pure-button-primary">Tout savoir sur ce projet</a></p>
+	        </div>
+	        <div class="pure-u-1-2">
+		        <img src="assets/images/placeholder.png" class="pure-img">
+	        </div>
+	    	</div>
+    	</section>
+    	
+    	<!-- -->
+    	<section id="content-bloc-3" class="wrapper2">
+	    	<div class="pure-g">
+	        <div class="pure-u-1 center">
+		        <h2>Tip of the day</h2>
+		        <blockquote>
+			        <p>« Designer, make sure there is enough contrast between text and its background colour  »</p>
+		        </blockquote>
+		        <p><a href="#" class="pure-button pure-button-primary">Learn to design for the colour blind</a></p>
+	        </div>
+	    	</div>
+    	</section>
+    	
+    	<!-- -->
+    	<footer id="footer" class="wrapper1">
+	    	<div class="pure-g">
+	        <div class="pure-u-1-3">
+		        <nav class="pure-menu custom-restricted-width">
+			        <h3 class="pure-menu-heading">Visionary</h3>
+			        <ul class="pure-menu-list">
+				        <li class="pure-menu-item"><a href="#" class="pure-button pure-button-primary">Tester l'extension</a></li>
+				        <li class="pure-menu-item"><a href="#" class="pure-button pure-button-primary">Développer pour les daltoniens</a></li>
+				        <li class="pure-menu-item"><a href="#" class="pure-button pure-button-primary">Qu'est-ce que le daltonisme</a></li>
+			        </ul>
+		        </nav>
+		      </div>
+	        <div class="pure-u-1-3">
+		        <nav class="pure-menu custom-restricted-width">
+			        <h4 class="pure-menu-heading">Partners</h4>
+			        <ul class="pure-menu-list">
+				        <li class="pure-menu-item"><a href="#" class="pure-menu-link">UNamur</a></li>
+				        <li class="pure-menu-item"><a href="#" class="pure-menu-link">HEAJ</a></li>
+				        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Région Wallonne</a></li>
+				        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Dog Studio</a></li>
+				        <li class="pure-menu-item"><a href="#" class="pure-menu-link">ACCETICS</a></li>
+			        </ul>
+		        </nav>
+	        </div>
+	        <div class="pure-u-1-3">
+		        <nav class="pure-menu custom-restricted-width">
+			        <h4 class="pure-menu-heading">Network</h4>
+			        <ul class="pure-menu-list">
+				        <li class="pure-menu-item"><a href="#" class="pure-menu-link">W3C</a></li>
+				        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Facebook</a></li>
+			        </ul>
+		        </nav>
+	        </div>
+	    	</div>
+    	</footer>    	
     </body>
 </html>
