a82c56d788ec76135f2fc89618a42acb77e6b45f
Plennevaux Alexandre <aplennevaux@gmail.com>
2016-12-23 02:26:51 +0100

    précisions

--- src/index.php
+++ src/index.php
@@ -1,14 +1,17 @@
 <?php
 
+// Ne touche pas à cette ligne.
 $f3 = require 'vendor/bcosca/fatfree/lib/base.php';
 
-// Load configuration
+// F3 configuration (voir la doc)
 $f3->config('config.ini');
+
 
 // Définis tes routes ici:
 
 $f3->route('GET /',
     function($f3) {
+	    
 		$f3->set('meta_title','Visionary Homepage');
 		echo View::instance()->render('header.html');
 		echo View::instance()->render('partials/menu.inc.html');
