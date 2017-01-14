5319c614946c4aa4c3be97ab87264f13d7ab6c89
Plennevaux Alexandre <aplennevaux@gmail.com>
2017-01-15 00:27:33 +0100

    Controller for homepage

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// Ne touche pas à cette ligne.
+// Ne pas toucher à cette ligne.
 $f3 = require 'vendor/bcosca/fatfree/lib/base.php';
 
 // F3 configuration (voir la doc)
@@ -14,13 +14,8 @@ $f3->route('GET /',
 	    
 		$f3->set('meta_title','Visionary Homepage');
 		echo View::instance()->render('header.html');
-		echo View::instance()->render('partials/menu.inc.html');
-
-		echo "<p>salut benoit, ça va? yolo?</p>";
-		
-		//$f3->set('content','welcome.htm');
+		echo View::instance()->render('home.html');
 		echo View::instance()->render('footer.html');
-		?></body></html><?php
     }
 );
 $f3->run();
