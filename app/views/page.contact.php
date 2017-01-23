ffd7b0ddca1b3625c8e021057408748a4ec3b3b8
Benoit Vrins <benoit@spade.be>
2017-01-23 15:35:20 +0100

    traduction FR

--- app/views/page.contact.php
+++ app/views/page.contact.php
@@ -3,46 +3,44 @@
 		    <div class="pure-g">
 	        <div class="pure-u-1 center">
 		        <h1>Contacter Visionary</h1>
-		        <p>Fill in the form below or send us a mail at <a href="mailto:team@colour-blindness.org">team@colour-blindness.org</a>. We’ll get back to you soon.</p>
+		        <p>Complétez le formulaire ci-dessous ou adressez-nous un email à <a href="mailto:team@colour-blindness.org">team@colour-blindness.org</a>. Nous reviendrons vers vous dès que possible.</p>
 		        <form class="pure-form pure-form-stacked">
 			        <fieldset>
 			            <legend>Legend</legend>
-			    
+
 			            <div class="pure-g">
 			                <div class="pure-u-1 pure-u-md-1-2">
 			                    <label for="first-name">Votre nom</label>
 			                    <input id="first-name" class="pure-u-23-24" type="text">
 			                </div>
-			    
+
 			                <div class="pure-u-1 pure-u-md-1-2">
 			                    <label for="email">Votre e-mail</label>
 			                    <input id="email" class="pure-u-23-24" type="email" required>
 			                </div>
 			            </div>
-			            
+
 			            <label for="state">Votre message</label>
 			            <textarea class="pure-input-1-2" placeholder="Je souhaiterais entrer en contact avec vous car..."></textarea>
-			    
+
 			            <button type="submit" class="pure-button pure-button-primary">Envoyer votre message</button>
 			        </fieldset>
 			    </form>
 	        </div>
 	    	</div>
 	    </section>
-	        
+
 	    <!-- -->
     	<section id="content-bloc-1" class="wrapper2">
 	    	<div class="pure-g">
 	        <div class="pure-u-1-3">
-		        
+
 		      </div>
 	        <div class="pure-u-1-3">
-		        
+
 		      </div>
 	        <div class="pure-u-1-3">
-		        
+
 		      </div>
 	    	</div>
     	</section>
-    	
-    	
